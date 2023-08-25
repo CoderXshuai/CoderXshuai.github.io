1bc808ab74722e7de700cafc382046d9d352a2c3
CoderXshuai <xshuai1024@foxmail.com>
2023-08-25 19:47:59 +0800

    优化布局完成

---
+++ typing-cn/assets/wordTool.5ecf14f3.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index.7a008dab.js";const o={shuffle(e){return e},getWords(e,r){return e!=="1"&&r&&r.length!==0?o.jsonCopy(r):o.jsonCopy(t)},jsonCopy(e){return JSON.parse(JSON.stringify(e))}};export{o as w};
